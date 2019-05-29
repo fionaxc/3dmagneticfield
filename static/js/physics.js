f0829c631f0fee75af441c6f533eda4f722b49a9
ccai1 <ccai1@stuy.edu>
2019-05-29 14:45:56 -0400

    added canvas

--- static/js/physics.js
+++ static/js/physics.js
@@ -1,1 +1,12 @@
-console.log("hi")
+var c = document.getElementById("playground");
+var ctx = c.getContext("2d");
+var clear = document.getElementById("clear");
+
+clear.addEventListener('click', function(e){
+  ctx.clearRect(0, 0, c.clientWidth, c.clientHeight)
+    }
+)
+
+c.addEventListener('click', function(e){
+    console.log("clicked")
+}
