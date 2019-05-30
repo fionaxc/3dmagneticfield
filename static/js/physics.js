a6bfa2735989815fc85a0930ad1e48f9301c05f0
ccai1 <ccai1@stuy.edu>
2019-05-30 14:12:10 -0400

    setup js

--- static/js/physics.js
+++ static/js/physics.js
@@ -1,12 +1,24 @@
 var c = document.getElementById("playground");
 var ctx = c.getContext("2d");
-var clear = document.getElementById("clear");
+var reset = document.getElementById("reset");
+var rectWidth = 600;
+var rectHeight = 600;
+var smth = document.getElementById("smth");
+var opt = document.getElementById("options")
 
-clear.addEventListener('click', function(e){
-  ctx.clearRect(0, 0, c.clientWidth, c.clientHeight)
-    }
+reset.addEventListener('click', function(e){
+  ctx.fillStyle = "#000000";
+  ctx.fillRect(0, 0, rectWidth, rectHeight);
+}
+)
+
+smth.addEventListener('click', function(e){
+  ctx.fillStyle = "#ADD8E6";
+  ctx.fillRect(0, 0, rectWidth, rectHeight);
+}
 )
 
 c.addEventListener('click', function(e){
-    console.log("clicked")
+  console.log(opt)
 }
+)
